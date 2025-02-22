e710367d47a0ac73c06dee2ba7fd8fdbcaeb6260
Stephen <stephen@Stephens-MacBook-Pro-2.local>
2025-02-22 16:37:29 -0500

    install some node packages

--- frontend/src/app/homepage_components/ui/chart.tsx
+++ frontend/src/app/homepage_components/ui/chart.tsx
@@ -138,8 +138,10 @@ const ChartTooltipContent = React.forwardRef<
         return null
       }
 
-      const [item] = payload
-      const key = `${labelKey || item.dataKey || item.name || "value"}`
+      const [item] = payload || []
+
+      const key = `${labelKey || (item ? item.dataKey || item.name : "value")}`
+      
       const itemConfig = getPayloadConfigFromPayload(config, item, key)
       const value =
         !labelKey && typeof label === "string"
