3d119afe0f4d98c59aff93079c8f21c9a95bf19f
StarchyPhantom <benjaminhuynh341115160@gmail.com>
2025-02-22 14:39:31 -0500

    Map added

---
+++ frontend/src/app/features/page.tsx
@@ -0,0 +1,51 @@
+import {
+    Shield,
+    Award,
+    Droplet,
+    Zap,
+    Flame,
+    Cloud,
+    Bot,
+    CheckCircle,
+  } from "lucide-react";
+  
+  import { WaterDrops } from "@/components/waterdrops";
+import GoogleMapsComponent from "./GoogleMap";
+  
+  const iconComponents = {
+    Shield,
+    Award,
+    Droplet,
+    Zap,
+    Flame,
+    Cloud,
+  };
+  
+  
+  export default function FeaturesPage() {
+    return (
+      <div className="min-h-screen bg-gradient-to-br from-teal-100 to-blue-200">
+        <div className="container mx-auto px-4 py-16">
+          <WaterDrops />
+          <h1 className="mb-12 text-center text-4xl font-bold text-teal-800">
+            Features
+          </h1>
+  
+          {/* <Map box> */}
+          <div className="mb-12 rounded-lg bg-white p-6 shadow-lg justify-center">
+            <div className="mb-4 flex items-center space-x-4">
+              <Bot className="h-8 w-8 text-teal-500" />
+              <h2 className="text-2xl font-semibold text-teal-800">
+                Local Shower Locator
+              </h2>
+            </div>
+            <h1>Map of showers</h1>
+            <GoogleMapsComponent />
+          </div>
+          
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          </div>
+        </div>
+      </div>
+    );
+  }
